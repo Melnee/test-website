86fc33e2496f10765dd3599f3b3820fbf3e8d02a
melnee <Melanie.Neilan@gmail.com>
2021-03-07 13:19:57 -0800

    initialized the gatsby website, made home/about pages

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from 'gatsby';
+
+
+export default () => (
+    <>
+	    <h1>Hello!</h1>
+        <p>Hello Minnesota</p>
+        <Link to= "/about">Learn about me &rarr;</Link>
+    </>
+);
